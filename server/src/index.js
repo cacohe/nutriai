9d20d9b49003d19f78744a07b741fadbf94244b5
cacohe <2568545091@qq.com>
2026-03-23 11:11:48 +0800

    add: 请求日志中间件

--- server/src/index.js
+++ server/src/index.js
@@ -8,6 +8,11 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+    console.log(`${new Date().toISOString()} ${req.method} ${req.path}`);
+    next();
+});
+
 app.use('/api', analysisRouter);
 
 app.get('/health', (req, res) => {
